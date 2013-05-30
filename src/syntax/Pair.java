046dad3d23f8efe88afe25b22560539421976518
Peng Wang <pengxlyw@gmail.com>
2013-05-30 22:58:13 +0800

    add equal for pairValue

--- src/syntax/Pair.java
+++ src/syntax/Pair.java
@@ -16,16 +16,4 @@ public class Pair extends Expression{
 		Value s = (Value) e2.eval();
 		return new PairValue(f,s);
 	}
-	public Expression getE1() {
-		return e1;
-	}
-	public void setE1(Expression e1) {
-		this.e1 = e1;
-	}
-	public Expression getE2() {
-		return e2;
-	}
-	public void setE2(Expression e2) {
-		this.e2 = e2;
-	}
 }
--- src/syntax/PairValue.java
+++ src/syntax/PairValue.java
@@ -16,4 +16,17 @@ public class PairValue extends Value{
 	public Value eval() {
 		return this;
 	}
+	
+	public BoolValue equal(Value anValue){
+		if (anValue == null){
+			return new BoolValue(false);
+		}
+		if (anValue instanceof PairValue){
+			PairValue anP = (PairValue) anValue;
+			boolean fst = (first.equal(anP.first)).value;
+			boolean snd = (second.equal(anP.second)).value;
+			return new BoolValue(fst&&snd);
+		}
+		return new BoolValue(false);
+	}
 }
